ff42be6a3ff4ed6632a00743cc826832698e8658
BrahimElhajji <elhajji080898@gmail.com>
2024-05-20 19:07:32 -0700

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Exporting an information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
+
+
+def get_team_member_task_list(team_member_id):
+    api_url = "https://jsonplaceholder.typicode.com/"
+    team_member_data = requests.get(api_url + "users/{}".format(
+        team_member_id)).json()
+    task_list = requests.get(
+            api_url + "todos", params={"userId": team_member_id}).json()
+
+    with open('{}.json'.format(team_member_id), 'w') as jsonfile:
+        json_data = {"USER_ID": []}
+        for task in task_list:
+            json_data["USER_ID"].append({
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": team_member_data.get("username")
+            })
+        json.dump(json_data, jsonfile)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python 2-export_to_JSON.py <team_member_id>")
+        sys.exit(1)
+
+    team_member_id = int(sys.argv[1])
+    get_team_member_task_list(team_member_id)

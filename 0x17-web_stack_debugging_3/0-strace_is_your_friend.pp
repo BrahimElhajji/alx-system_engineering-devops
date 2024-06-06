fad6964f0ac2341dca0e123f0367df61cc1c30a4
BrahimElhajji <elhajji080898@gmail.com>
2024-06-05 18:28:57 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,7 @@
 #Fixing a Permissions Issue
 
-exec { 'strace-apache':
-  command => "strace -f -e trace=network -s 1000 -p $(pidof apache2) 2>&1 | grep -i 'error'",
-  path    => '/usr/bin/',
-}
-
-exec { 'fix-wordpress':
-  command => "sudo find / -name 'wp-config.php' -exec sed -i 's/define(\\'DB_CHARSET\\', \\''\\');/define(\\'DB_CHARSET\\', \\'utf8\\');/g' {} +",
-  path    => '/usr/bin/',
-  require => Exec['strace-apache'],
+exec { 'fix_db_charset':
+  command => 'sed -i "s/define(\\\'DB_CHARSET\\\', \\'\\');/define(\\\'DB_CHARSET\\\', \\'utf8\\');/g" /var/www/html/wp-config.php',
+  path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+  onlyif  => 'grep -q "define(\\\'DB_CHARSET\\\', \\'\\');" /var/www/html/wp-config.php',
 }

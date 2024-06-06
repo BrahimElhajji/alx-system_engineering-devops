17e5359e8eefad2baeaf4f73a7da062aaacdb818
BrahimElhajji <elhajji080898@gmail.com>
2024-06-05 18:26:35 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,12 @@
 #Fixing a Permissions Issue
 
-exec { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  content => template('wp-settings.php.erb'),
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
+exec { 'strace-apache':
+  command => "strace -f -e trace=network -s 1000 -p $(pidof apache2) 2>&1 | grep -i 'error'",
+  path    => '/usr/bin/',
 }
 
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/var/www/html/wp-settings.php'],
+exec { 'fix-wordpress':
+  command => "sudo find / -name 'wp-config.php' -exec sed -i 's/define(\\'DB_CHARSET\\', \\''\\');/define(\\'DB_CHARSET\\', \\'utf8\\');/g' {} +",
+  path    => '/usr/bin/',
+  require => Exec['strace-apache'],
 }

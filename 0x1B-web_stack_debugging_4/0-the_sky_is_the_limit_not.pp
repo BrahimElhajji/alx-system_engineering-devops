8b8ceb09b95425e789d579f2bece75a2f9d659b7
BrahimElhajji <elhajji080898@gmail.com>
2024-06-13 20:07:17 -0700

    Web stack debugging 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,44 +1,14 @@
-# Define the nginx class
-class nginx {
-  # Ensure the nginx package is installed and present
-  package { 'nginx':
-    ensure => present,
-  }
+# Increase the maximum number of connections for Nginx
 
-  # Ensure the nginx service is running, enabled, and restarted if necessary
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    restart => true,
-  }
+# Update the default Nginx configuration file to increase the ULIMIT
+exec { 'increase-nginx-ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin/', '/bin/'],
+}
 
-  # Configure the default Nginx configuration file
-  file { '/etc/nginx/sites-available/default':
-    # Set the content of the file to the desired server block configuration
-    content => '
-      server {
-        # Listen on port 80
-        listen 80;
-        # Set the server name to localhost
-        server_name localhost;
-        # Define the location block for the root URL (/)
-        location / {
-          # Proxy requests to http://localhost:8080
-          proxy_pass http://localhost:8080;
-          # Set the proxy HTTP version to 1.1
-          proxy_http_version 1.1;
-          # Set the Upgrade header to allow WebSocket connections
-          proxy_set_header Upgrade $http_upgrade;
-          # Set the Connection header to "upgrade" for WebSocket connections
-          proxy_set_header Connection \'upgrade\';
-          # Set the Host header to the original host
-          proxy_set_header Host $host;
-          # Bypass proxy caching for WebSocket connections
-          proxy_cache_bypass $http_upgrade;
-        }
-      }
-    ',
-    # Notify the nginx service to restart if the configuration file changes
-    notify  => Service['nginx'],
-  }
+# Restart Nginx to apply the changes
+exec { 'restart-nginx':
+  command => 'service nginx restart',
+  path    => ['/etc/init.d/'],
+  require => Exec['increase-nginx-ulimit'],
 }
